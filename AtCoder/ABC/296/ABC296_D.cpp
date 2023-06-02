881a9f82cf013b889ac0fe1c959f0edb24ad80dd
tetraloba <four.leaves.siva7342@gmail.com>
2023-06-03 00:20:51 +0900

    ABC296_D

---
+++ AtCoder/ABC/296/ABC296_D.cpp
@@ -0,0 +1,23 @@
+// 2023/06/03 00:04:50
+// 
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int N, M;
+    cin >> N >> M;
+    int n1_l = 0, n2_l = 0;
+    int n1_r = N, n2_r = N;
+    int n1_m = (n1_l + n1_r) / 2;
+    int n2_m = (n2_l + n2_r) / 2;
+    if ()
+    cout <<  << endl;
+    return 0;
+}
+// 2分探索で近いところ見つけられる…？ nlog(n)では遅いのか・・・
+
+8 7
+4 4 16
+2 4 8
+2 2 4
+2 3 6
