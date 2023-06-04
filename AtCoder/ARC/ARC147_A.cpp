b13eec5efbfced745535667f8cc8c13249b7ed38
tetraloba <four.leaves.siva7342@gmail.com>
2023-06-04 21:28:14 +0900

    ARC147_A

---
+++ AtCoder/ARC/ARC147_A.cpp
@@ -0,0 +1,24 @@
+// 2023/06/04 16:49:43
+// 2023/06/04 17:04:36 無理だ。分からん。
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Cell{
+    int value;
+    Cell *next;
+};
+int main(){
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    Cell *head;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        cell;
+    }
+    cout <<  << endl;
+    return 0;
+}
+
+// すごく計算量のことを考えさせられる問題だなあ。好き。
