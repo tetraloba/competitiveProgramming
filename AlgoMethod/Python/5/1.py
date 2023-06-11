cc143853fc669a82d95d80428bc7b3ae765575ab
tetraloba <four.leaves.siva7342@gmail.com>
2023-06-11 09:24:27 +0900

    AM_Python_5_1to8

---
+++ AlgoMethod/Python/5/1.py
@@ -0,0 +1,7 @@
+# 2023/06/11 08:50:27
+# 2023/06/11 08:52:06
+numbers = [3, 14, 15, 92, 65, 35]
+sum = 0
+for number in numbers:
+    sum += number
+print(sum)
---
+++ AlgoMethod/Python/5/4.py
@@ -0,0 +1,8 @@
+# 2023/06/11 08:58:19
+# 
+for n in range(1, 11):
+    str = ""
+    for t in range(n):
+        str += "o"
+    print(str)
+
