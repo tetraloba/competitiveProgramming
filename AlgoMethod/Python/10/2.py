2872e02fdfeb6abd7e0abe69c5434f8d091aa323
tetraloba <four.leaves.siva7342@gmail.com>
2023-06-25 22:40:48 +0900

    AM_Python_10_1to2

---
+++ AlgoMethod/Python/10/2.py
@@ -0,0 +1,8 @@
+# 2023/06/25 22:39:05
+# 2023/06/25 22:40:35 AC.
+# 非常に長い 2 つの文字列
+sentence1 = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum"
+sentence2 = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exersitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum"
+
+# 文字列の id が同じかどうかを判定し出力するコードをここに書いてください
+print('True' if sentence1 is sentence2 else 'False')
