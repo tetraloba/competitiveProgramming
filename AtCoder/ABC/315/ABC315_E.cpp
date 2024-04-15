22869c06a458cfa234f61681261ad73e5f151608
tetraloba <four.leaves.siva7342@gmail.com>
2024-04-15 23:03:54 +0900

    ABC315_E

--- AtCoder/ABC/315/ABC315_E.cpp
+++ AtCoder/ABC/315/ABC315_E.cpp
@@ -1,7 +1,8 @@
 // 2024/04/15 21:03:24
 // 2024/04/15 21:22:21 24WA
 // 2024/04/15 21:48:35 24WA
-// 
+// 2024/04/15 22:26:29 TLE
+// 2024/04/15 22:49:51 AC.
 #include <iostream>
 #include <vector>
 #include <set>
@@ -11,62 +12,52 @@ using namespace std;
 int main(){
     int N;
     cin >> N;
-    vector<set<int>> P(N);
+    vector<int> C(N);
+    vector<set<int>> P(N), rP(N);
     for (int i = 0; i < N; i++) {
-        int c;
-        cin >> c;
-        for (int j = 0; j < c; j++) {
+        cin >> C[i];
+        for (int j = 0; j < C[i]; j++) {
             int p;
             cin >> p;
             p--;
             P[i].insert(p);
+            rP[p].insert(i);
         }
     }
 
-    for (auto st : P) {
-        for (int i : st) {
-            cout << i << ' ';
-        }
-        cout << endl;
-    }
-
+    vector<bool> part_of_0(N);
     queue<int> que;
-    vector<int> parent(N, -1);
+    /* 0の前提となる本の集合(part_of_0[i] == true)を求める */
     que.push(0);
+    part_of_0[0] = true;
     while (!que.empty()) {
         int tgt = que.front(); que.pop();
         for (int next : P[tgt]) {
-            if (parent[next] == -1) {
+            if (!part_of_0[next]) {
                 que.push(next);
-            } else {
-                P[parent[next]].erase(next);
+                part_of_0[next] = true;
             }
-            parent[next] = tgt;
         }
     }
-    // ここまでの処理で木になっているはず。
 
-    for (auto st : P) {
-        for (int i : st) {
-            cout << i << ' ';
+    /* 0の前提集合で前提本が無い本を最初に読む */
+    for (int i = 0; i < N; i++) {
+        if (part_of_0[i] && !C[i]) {
+            que.push(i);
         }
-        cout << endl;
     }
-
-    vector<int> rseq;
-    que.push(0);
-    int t = 0; // time
     while (!que.empty()) {
-        t--;
         int tgt = que.front(); que.pop();
-        for (int next : P[tgt]) {
-            rseq.push_back(next);
-            que.push(next);
+        if (tgt) { // (0は出力しない)
+            cout << tgt + 1 << ' ';
         }
-    }
-    vector<int> seq(rseq.rbegin(), rseq.rend());
-    for (int id : seq) {
-        cout << id + 1 << ' ';
+        for (int next : rP[tgt]) {
+            C[next]--; // 前提本を1つ(tgt)読み終わった
+            /* 前提本を全て読み終わったなら読書キューに追加 */
+            if (part_of_0[next] && !C[next]) {
+                que.push(next);
+            }
+        }
     }
     cout << endl;
     return 0;
