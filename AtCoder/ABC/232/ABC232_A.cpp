d067880c2312ee295f9a4151e29b7946f774a4dc
tetraloba <four.leaves.siva7342@gmail.com>
2025-04-01 13:55:55 +0900

    ABC232_A

---
+++ AtCoder/ABC/232/ABC232_A.cpp
@@ -0,0 +1,13 @@
+// 2025/04/01 13:47
+// 2025/04/01 13:51 AC.
+#include <iostream>
+using namespace std;
+int main(){
+	string S;
+	cin >> S;
+	int a = S[0] - '0';
+	int b = S[2] - '0';
+	cout << a * b << endl;
+	return 0;
+}
+
