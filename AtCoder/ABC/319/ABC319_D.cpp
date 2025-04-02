d2706e5f1cdff6926eb4ce1de5df96cc9f7186e2
tetraloba <four.leaves.siva7342@gmail.com>
2025-04-02 15:20:54 +0900

    ABC319_D_giveup

---
+++ AtCoder/ABC/319/ABC319_D.cpp
@@ -0,0 +1,12 @@
+// 2025/03/08 11:59:06
+// 2025/03/08 12:28:35 give up
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int N, M;
+    cin >> N >> M;
+    cout <<  << endl;
+    return 0;
+}
+
